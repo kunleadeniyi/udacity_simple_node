e612bcbd18b10f54c83c34ebb16553e43e8661ce
Ayokunle Adeniyi <Ayokunle.Adeniyi@warwick.ac.uk>
2023-01-21 23:52:59 +0000

    update server.js file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const terminalOutput = process.env.TERMINAL_OUTPUT || 'Docker Containers rule!'
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(terminalOutput);
     await sleep(5000);
   }
 }
